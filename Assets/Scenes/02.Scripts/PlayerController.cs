f0f8c50e148101d69f0f97247cdb8f623d5049b4
MinJi Kang <86655689+100100001001@users.noreply.github.com>
2022-04-06 11:59:19 +0900

    Edit DragAndDrop_Positional dress up

--- Assets/Scenes/02.Scripts/PlayerController.cs
+++ Assets/Scenes/02.Scripts/PlayerController.cs
@@ -4,11 +4,27 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        switch ()
-        {
+    public GameObject hat1;
+    public GameObject ribbon1;
+    public GameObject bag1;
 
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("¿Í!");
+
+        switch (collision.tag)
+        {
+            case "hat1":
+                hat1.SetActive(hat1.activeSelf ? false : true);
+                break;
+            case "ribbon1":
+                ribbon1.SetActive(ribbon1.activeSelf ? false : true);
+                break;
+            case "bag1":
+                bag1.SetActive(bag1.activeSelf ? false : true);
+                break;
+            default:
+                break;
         }
     }
 }
