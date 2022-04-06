4e2d2e732c7e519b91cc30ef89f24b451fe3a804
MinJi Kang <86655689+100100001001@users.noreply.github.com>
2022-04-07 00:29:01 +0900

    Add function revert to the first (Scene Reload)

--- Assets/Scenes/02.Scripts/GameManager.cs
+++ Assets/Scenes/02.Scripts/GameManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -25,5 +26,10 @@ public class GameManager : MonoBehaviour
             default:
                 break;
         }
+    }
+
+   public void Reload()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
